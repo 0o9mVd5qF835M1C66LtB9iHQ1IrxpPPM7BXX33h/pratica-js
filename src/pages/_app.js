abf7011757dacc3dba5672d2f529b5e50402c576
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-05-07 15:16:21 -0300

    :sparkles:	feat: add Footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,22 @@
-import '@/styles/globals.scss'
+import "@/styles/globals.scss";
 
-import { Navbar } from '@/components/Navbar'
-import { Poppins } from 'next/font/google'
+import { Navbar } from "@/components/Navbar";
+import { Poppins } from "next/font/google";
+import { Footer } from "@/components/Footer";
 
 const poppins = Poppins({
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin']
-})
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <div className={`${poppins.className} bg-zinc-900`}>
       <Navbar />
       <Component {...pageProps} />
+
+      <Footer />
     </div>
-  )
+  );
 }
