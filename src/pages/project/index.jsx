bc18eeb600dbbfca1dd0f6fa7dbfb05381430c3e
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-05-14 10:12:13 -0300

    :lipstick:	feat: news text

--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -1,37 +1,33 @@
-import { PostCard } from '@/components/PostCard'
-import { Categories } from '@/components/Categories'
-import { PostWidget } from '@/components/PostWidget'
+import { PostCard } from "@/components/PostCard";
+import { PostWidget } from "@/components/PostWidget";
 
-import { getPosts } from '@/services'
+import { getPosts } from "@/services";
 
 export default function Projects({ posts }) {
   return (
     <>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg-px-8 mt-10l flex flex-col items-center mt-10">
-        <h2 className="font-bold text-transparent text-3xl lg:text-5xl bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-          Publicados recentemente
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg-px-8 mt-10l flex flex-col  mt-10">
+        <h2 className="font-semibold text-4xl text-primary-900 text-left bg-gradient-to-r from-yellow-200 via-yellow-300 to-yellow-400 bg-clip-text text-transparent">
+          Projetos
         </h2>
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <p className="border-l-2 pl-2 mt-6 text-zinc-500">
+          Sinta-se a vontade para escolher o melhor desafio que se encaixa com a
+          sua busca para alcançar o próximo nível!
+        </p>
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 mt-10">
           {posts.map((post) => (
             <PostCard post={post.node} key={post.title} />
           ))}
         </div>
       </div>
-
-      {/* <div className="lg:col-span-4 col-span-1">
-        <div className="lg:sticky relative top-8 border">
-          <PostWidget />
-          <Categories />
-        </div>
-      </div> */}
     </>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const posts = (await getPosts()) || []
+  const posts = (await getPosts()) || [];
 
   return {
-    props: { posts }
-  }
+    props: { posts },
+  };
 }
