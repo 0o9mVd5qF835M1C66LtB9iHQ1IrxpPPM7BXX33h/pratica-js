5f4fa17b01eac7d310e2bd26a853773905da58bf
viniciusoliver-stack <vinicius.so.contato@hotmail.com>
2023-05-14 10:12:45 -0300

    :lipstick:	feat: changing font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,18 @@
 import "@/styles/globals.scss";
 
 import { Navbar } from "@/components/Navbar";
-import { Poppins } from "next/font/google";
+import { Inter } from "next/font/google";
 import { Footer } from "@/components/Footer";
 
-const poppins = Poppins({
+const lato = Inter({
   weight: ["400", "700"],
-  style: ["normal", "italic"],
+  style: ["normal"],
   subsets: ["latin"],
 });
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className={`${poppins.className} bg-zinc-900`}>
+    <div className={`${lato.className} bg-zinc-900`}>
       <Navbar />
       <Component {...pageProps} />
 
